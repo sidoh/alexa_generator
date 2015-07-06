a82047a6ffeedcc6332231a5c2993f7e2f851523
Christopher <cmullins@liveramp.com>
2015-07-05 22:42:17 -0700

    add builder and spec for intent

--- lib/alexa_generator/intent.rb
+++ lib/alexa_generator/intent.rb
@@ -5,23 +5,37 @@ module AlexaGenerator
     attr_reader :name, :slots
 
     class Builder
+      attr_reader :bindings
+
       def initialize(name)
         @name = name
         @slots = []
+        @bindings = {}
       end
 
-      def add_slot(name, type)
-        @slots.push( Slot.new(name, type) )
+      def add_slot(name, type, &block)
+        builder = Slot.build(name, type, &block)
+
+        @bindings[name] ||= []
+        @bindings[name].concat(builder.bindings)
+
+        @slots.push(builder.create)
       end
 
       def create
-        Intent.new(name, slots)
+        Intent.new(@name, @slots)
       end
     end
 
     def initialize(name, slots)
       @name = name
       @slots = slots
+    end
+
+    def self.build(name, &block)
+      builder = Builder.new(name)
+      block.call(builder)
+      builder
     end
   end
 end
---
+++ spec/alexa_generator/intent_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe AlexaGenerator::Slot do
+  context 'builder' do
+    it 'should return an instance of a builder' do
+      slot = AlexaGenerator::Intent.build(:IntentOne) { |x| }
+
+      expect(slot).to be_an_instance_of(AlexaGenerator::Intent::Builder)
+    end
+
+    it 'should add slot bindings' do
+      intent = AlexaGenerator::Intent.build(:IntentOne) do |intent|
+        intent.add_slot(:SlotOne, AlexaGenerator::Slot::SlotType::LITERAL) do |slot|
+          slot.add_bindings('a', 'b', 'c')
+        end
+      end
+
+      expect(intent.bindings).to eq({SlotOne: ['a', 'b', 'c']})
+    end
+
+    it 'should create an intent when create is called' do
+      builder = AlexaGenerator::Intent.build(:IntentOne) do |intent|
+        intent.add_slot(:SlotOne, AlexaGenerator::Slot::SlotType::LITERAL) do |slot|
+          slot.add_bindings('a', 'b', 'c')
+        end
+      end
+
+      intent = builder.create
+
+      expect(intent.name).to eq(:IntentOne)
+
+      expect(intent.slots.count).to eq(1)
+      expect(intent.slots.first.name).to eq(:SlotOne)
+    end
+  end
+end
